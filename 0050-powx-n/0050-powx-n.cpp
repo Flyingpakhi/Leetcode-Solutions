d15ce5582476144016cd28297abb31ca3cb30495
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-10-17 19:59:17 +0530

    Time: 23 ms (8.05%), Space: 35.2 MB (16.02%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    double power(double x,int n){
+        if(n==0){
+            return 1.0;
+        }
+        double small=power(x,n-1);
+        if(small>INT_MAX/x || small<INT_MIN/x){
+            return 0;
+        }
+        double big=x*small;
+        return big;
+    }
+    double myPow(double x, int n) {
+        if(n>=INT_MAX){
+            return x;
+        }
+        if(n<=INT_MIN){
+            double ans=1/x;
+            int ans1=ans;
+            if(ans<0)
+            return (-1)*double(ans1);
+            else
+                return double(ans1);
+        }
+        if(n>0){
+            if(x>0)
+        return power(x,n);
+            else
+                x=x*(-1);
+            if(n%2==0){
+                return power(x,n);
+            }
+            else{
+                return (-1)*power(x,n);
+            }
+    }
+        else{
+        if(x>0){
+            n=n*(-1);
+            return 1/power(x,n);
+        }
+            else{
+                x=x*(-1);
+                n=n*(-1);
+               if(n%2==0){
+                return 1/power(x,n);
+            }
+            else{
+                return (-1)/power(x,n);
+            } 
+            }
+        }
+}
+};
