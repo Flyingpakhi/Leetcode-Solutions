5969994b6991eed5577700c00a0a97d2a808fe67
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-10-19 09:55:39 +0530

    Time: 7 ms (38.15%), Space: 16.8 MB (16.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,26 @@
 class Solution {
     private:
-    void solve(vector<int> nums,int index,vector<int>output,vector<vector<int>>& ans){
+    void solve(vector<int> nums,int index,vector<int>output,vector<vector<int>>& ans,int &count){
         if(index>=nums.size()){
             ans.push_back(output);
+            count++;
             return ;
         }
         //exclude
-        solve(nums,index+1,output,ans);
+        solve(nums,index+1,output,ans,count);
         //include
         int element=nums[index];
         output.push_back(element);
-        solve(nums,index+1,output,ans);
+        solve(nums,index+1,output,ans,count);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> output;
         int index=0;
-        solve(nums,index,output,ans);
+        int count=0;
+        solve(nums,index,output,ans,count);
+        cout<<count<<endl;
         return ans;
     }
 };
