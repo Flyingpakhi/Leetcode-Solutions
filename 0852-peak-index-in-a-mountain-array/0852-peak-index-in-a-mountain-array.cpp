5554314fe6c78bcaebf01eda8d3b8b895552f9d4
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-11-14 10:16:44 +0530

    Time: 357 ms (23.25%), Space: 59.7 MB (18.29%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,17 +3,15 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int low=0;
         int high=arr.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        int mid=low+(high-low)/2;
+        while(low<high){
             if(arr[mid]<arr[mid+1]){
                 low=mid+1;
-            }
-            else if(arr[mid]>arr[mid+1]){
-                high=mid-1;
             }
             else{
                 high=mid;
             }
+            mid=low+(high-low)/2;
         }
         return low;
     }
