629b3955cab5bc15500fa8a8139dbd52219a38e1
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-11-15 09:47:53 +0530

    Time: 11 ms (14.07%), Space: 8.7 MB (37.95%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>ans;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans.push_back(sum);
+        }
+        // for(int i=0;i<ans.size();i++){
+        //     cout<<ans[i]<<endl;
+        // }
+        return ans;
+    
+    }
+};
