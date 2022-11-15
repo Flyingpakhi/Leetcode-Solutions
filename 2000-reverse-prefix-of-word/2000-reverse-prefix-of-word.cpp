8698e014ec3d9a9c80773dec0cbb27786ee6d9f1
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-11-15 23:40:57 +0530

    Time: 3 ms (55.48%), Space: 6.1 MB (75.28%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int count=0;
-        for(int i=0;word[i]!='\0';i++){
-            if(word[i]==ch){
-                count=i;
-                break;
-            }
-        }
-    int i=0,j=count;
-    while(i<j){
-	char temp;
-        temp=word[i];
-        word[i]=word[j];
-        word[j]=temp;
-        i++;
-        j--;
-        }
+       for(int i=0;word[i]!='\0';i++){
+           if(word[i]==ch){
+               int k=0;
+               int j=i;
+               while(k<j){
+                   char temp=word[k];
+                   word[k]=word[j];
+                   word[j]=temp;
+                   k++;
+                   j--;
+               }
+               break;
+           }
+       }
         return word;
     }
 };
