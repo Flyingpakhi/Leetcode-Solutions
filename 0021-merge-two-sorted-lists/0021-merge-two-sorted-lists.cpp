e6027dcd1bd5b5280e238c39a4215816702fd75f
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-12-05 10:02:25 +0530

    Time: 24 ms (7.92%), Space: 14.8 MB (42.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,32 +11,71 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       ListNode* temp_node=new ListNode(0);
-        ListNode* currnode=temp_node;
-        ListNode* head=temp_node;
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
-                currnode->next=list1;
-                list1=list1->next;
-                currnode=currnode->next;
-            }
-            else{
-                currnode->next=list2;
-                list2=list2->next;
-                currnode=currnode->next;
-            }
-        }
-        while(list1!=NULL){
-            currnode->next=list1;
+       // ListNode* temp_node=new ListNode(0);
+       //  ListNode* currnode=temp_node;
+       //  ListNode* head=temp_node;
+       //  while(list1!=NULL && list2!=NULL){
+       //      if(list1->val<list2->val){
+       //          currnode->next=list1;
+       //          list1=list1->next;
+       //          currnode=currnode->next;
+       //      }
+       //      else{
+       //          currnode->next=list2;
+       //          list2=list2->next;
+       //          currnode=currnode->next;
+       //      }
+       //  }
+       //  while(list1!=NULL){
+       //      currnode->next=list1;
+       //      list1=list1->next;
+       //      currnode=currnode->next;
+       //  }
+       //  while(list2!=NULL){
+       //      currnode->next=list2;
+       //      list2=list2->next;
+       //      currnode=currnode->next;
+       //  }
+       //  head=head->next;
+       //  return head;
+    if(list1==NULL){
+		return list2;
+	}
+	if(list2==NULL){
+		return list1;
+	}
+    ListNode* fHead=NULL;
+	if(list1->val<list2->val){
+		fHead=list1;
+		list1=list1->next;
+	}
+	else{
+		fHead=list2;
+		list2=list2->next;
+	}
+	ListNode* fTail=fHead;
+	while(list1!=NULL && list2!=NULL){
+		if(list1->val<list2->val){
+			fTail->next=list1;
+			fTail=fTail->next;
             list1=list1->next;
-            currnode=currnode->next;
+		}
+		else{
+			fTail->next=list2;
+            fTail = fTail->next;
+            list2 = list2->next;
+            }
+	}
+    while (list1!= NULL) {
+          fTail->next = list1;
+          fTail = fTail->next;
+          list1 = list1->next;
         }
-        while(list2!=NULL){
-            currnode->next=list2;
-            list2=list2->next;
-            currnode=currnode->next;
+    while (list2 != NULL) {
+          fTail->next = list2;
+          fTail = fTail->next;
+          list2 = list2->next;
         }
-        head=head->next;
-        return head;
+		return fHead;
     }
 };
