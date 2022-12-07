9c3f568ee61e5f88c9e4c519d8aa89f8a61e9ab5
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-12-07 09:47:56 +0530

    Time: 81 ms (18.64%), Space: 71.6 MB (12.41%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,8 +16,16 @@ public:
         ListNode* curr=temp;
         ListNode* head=temp;
         int sum,digit,sum1;
-        while(l1!=NULL && l2!=NULL){
-            sum=l1->val+l2->val+carry;
+        while(l1!=NULL || l2!=NULL){
+            if(l1==NULL){
+                sum=l2->val+carry;
+            }
+            else if(l2==NULL){
+                sum=l1->val+carry;
+            }
+            else{
+                sum=l1->val+l2->val+carry;
+            }
             if(sum>9){
                 digit=sum%10;
                 carry=sum/10;
@@ -29,39 +37,41 @@ public:
             ListNode* newnode=new ListNode(digit);
                 curr->next=newnode;
                 curr=curr->next;
+            if(l1!=NULL)
                 l1=l1->next;
+            if(l2!=NULL)
                 l2=l2->next;
         }
-        while(l1!=NULL){
-            sum1=l1->val+carry;
-            if(sum1>9){
-                digit=sum1%10;
-                carry=sum1/10;
-            }
-            else{
-                digit=sum1;
-                carry=sum1/10;
-            }
-            ListNode* newnode=new ListNode(digit);
-            curr->next=newnode;
-            curr=curr->next;
-            l1=l1->next;
-        }
-        while(l2!=NULL){
-            sum1=l2->val+carry;
-            if(sum1>9){
-                digit=sum1%10;
-                carry=sum1/10;
-            }
-            else{
-                digit=sum1;
-                carry=sum1/10;
-            }
-            ListNode* newnode=new ListNode(digit);
-            curr->next=newnode;
-            curr=curr->next;
-            l2=l2->next;
-        }
+        // while(l1!=NULL){
+        //     sum1=l1->val+carry;
+        //     if(sum1>9){
+        //         digit=sum1%10;
+        //         carry=sum1/10;
+        //     }
+        //     else{
+        //         digit=sum1;
+        //         carry=sum1/10;
+        //     }
+        //     ListNode* newnode=new ListNode(digit);
+        //     curr->next=newnode;
+        //     curr=curr->next;
+        //     l1=l1->next;
+        // }
+        // while(l2!=NULL){
+        //     sum1=l2->val+carry;
+        //     if(sum1>9){
+        //         digit=sum1%10;
+        //         carry=sum1/10;
+        //     }
+        //     else{
+        //         digit=sum1;
+        //         carry=sum1/10;
+        //     }
+        //     ListNode* newnode=new ListNode(digit);
+        //     curr->next=newnode;
+        //     curr=curr->next;
+        //     l2=l2->next;
+        // }
         if(carry>0){
             ListNode* newnode=new ListNode(carry);
             curr->next=newnode;
