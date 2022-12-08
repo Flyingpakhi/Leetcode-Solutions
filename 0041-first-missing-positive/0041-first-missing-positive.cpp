3b04384fb2d8fc17ec7ed2cd2f80b0d512f6cd8b
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-12-08 22:48:53 +0530

    Time: 64 ms (81.55%), Space: 33.7 MB (27.18%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+       vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans.push_back(nums[i]);
+            }
+        }
+        vector<int>::iterator ip;
+        sort(ans.begin(),ans.end());
+        ip = std::unique(ans.begin(), ans.end());
+        ans.resize(std::distance(ans.begin(), ip));
+        int sum=0;
+        for(int i=0;i<ans.size();i++){
+            sum+=1;
+            if(sum!=ans[i]){
+                return sum;
+            }
+        }
+        return sum+1;
+    }
+};
