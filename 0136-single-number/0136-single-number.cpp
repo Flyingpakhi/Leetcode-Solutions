b3fec710f47a65ed4914e9796cd2f405f2bd7e8e
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2022-12-08 23:15:45 +0530

    Time: 59 ms (27.72%), Space: 16.9 MB (86.75%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int i;
+        for(i=0;i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+            else{
+                i++;
+            }
+        }
+        if(nums[i]!=nums[i-1]){
+            return nums[i];
+        }
+        return 0;
+    }
+};
