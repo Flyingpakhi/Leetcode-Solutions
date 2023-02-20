85044c8f25c1b980feb416932b969d83eac85320
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-02-20 09:40:30 +0530

    Time: 7 ms (37.22%), Space: 9.8 MB (31.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,24 +1,25 @@
-using namespace std;
 class Solution {
 public:
-    int binarySearch(vector<int> nums,int target){
+    int binarysearch(vector<int>& nums,int key){
         int s=0;
         int e=nums.size()-1;
+        int mid;
         while(s<=e){
-            int mid=s+(e-s)/2;
-            if(nums[mid]==target){
+            mid=s+(e-s)/2;
+            if(nums[mid]==key){
                 return mid;
             }
-            else if(nums[mid]>target){
+            else if(nums[mid]>key){
                 e=mid-1;
             }
-               else{
-                   s=mid+1;
-               }
+            else{
+                s=mid+1;
+            }
         }
-        return s;
+    return s;
     }
     int searchInsert(vector<int>& nums, int target) {
-        return binarySearch(nums,target);
+       int ans=binarysearch(nums,target); 
+        return ans;
     }
 };
