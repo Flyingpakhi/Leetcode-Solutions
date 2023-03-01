cff19dbb8213bcd73f9bba727d64de06d69f3150
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-03-01 08:13:52 +0530

    Time: 252 ms (43.95%), Space: 61.2 MB (65.89%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,59 +1,60 @@
 class Solution {
 public:
-void insert(vector<int>&nums) {
-  int i=1;
-  while(i<nums.size()){
-    int childIndex = i;
-    while (childIndex > 0) {
-      int parentIndex = (childIndex - 1) / 2;
-      if (nums[childIndex] > nums[parentIndex]) {
-        int temp = nums[childIndex];
-        nums[childIndex] = nums[parentIndex];
-        nums[parentIndex] = temp;
-      } else {
-        break;
-      }
-      childIndex = parentIndex;
+    void insert(vector<int>& nums){
+        int i=1;
+        while(i<nums.size()){
+            int childIndex=i;
+            while(childIndex>0){
+                int parentIndex=(childIndex-1)/2;
+                if(nums[childIndex]>nums[parentIndex]){
+                    int temp=nums[childIndex];
+                    nums[childIndex]=nums[parentIndex];
+                    nums[parentIndex]=temp;
+                }
+                else{
+                    break;
+                }
+                childIndex=parentIndex;
+            }
+            i++;
+        }
     }
-    i++;
-  }
-}
-void removeMax(vector<int>&nums) {
-  if (nums.size()==0) {
-    return ;
-  }
-int n=nums.size();
-  while(n!=0){
-  int ans = nums[0];
-  nums[0] = nums[n- 1];
-  nums[n-1]=ans;
-  n--;
-  int parIndex = 0;
-  int leftChild = 2 * parIndex + 1;
-  int rightChild = 2 * parIndex + 2;
-  while (leftChild <n) {
-    int maxIndex = parIndex;
-    if (nums[maxIndex] < nums[leftChild]) {
-      maxIndex = leftChild;
+    void remove(vector<int>& nums){
+        if(nums.size()==0){
+            return;
+        }
+        int n=nums.size();
+        while(n!=0){
+            int ans=nums[0];
+            nums[0]=nums[n-1];
+            nums[n-1]=ans;
+            n--;
+            int parentIndex=0;
+            int lc=2*parentIndex+1;
+            int rc=2*parentIndex+2;
+            while(lc<n){
+                int maxIndex=parentIndex;
+                if(nums[maxIndex]<nums[lc]){
+                    maxIndex=lc;
+                }
+                if(nums[maxIndex]<nums[rc]&& rc<n){
+                    maxIndex=rc;
+                }
+                if(maxIndex==parentIndex){
+                    break;
+                }
+                int temp=nums[maxIndex];
+                nums[maxIndex]=nums[parentIndex];
+                nums[parentIndex]=temp;
+                parentIndex=maxIndex;
+                lc=2*parentIndex+1;
+                rc=2*parentIndex+2;
+            }
+        }
     }
-    if (rightChild <n && nums[rightChild] >nums[maxIndex]) {
-      maxIndex = rightChild;
-    }
-    if (maxIndex == parIndex) {
-      break;
-    }
-    int temp = nums[maxIndex];
-    nums[maxIndex] = nums[parIndex];
-    nums[parIndex] = temp;
-    parIndex = maxIndex;
-    leftChild = 2 * parIndex + 1;
-    rightChild = 2 * parIndex + 2;
-  }
-  }
-}
     vector<int> sortArray(vector<int>& nums) {
         insert(nums);
-        removeMax(nums);
+        remove(nums);
         return nums;
     }
 };
