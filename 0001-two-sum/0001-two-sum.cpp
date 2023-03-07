e25530e59467156e907a17c4c3739bb16fcaf28d
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-03-07 21:02:20 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>output;
+        vector<int>outputSum;
        for(int i=0;i<nums.size();i++){
            for(int j=i+1;j<nums.size();j++){
                if(nums[i]+nums[j]==target){
-                   output.push_back(i);
-                   output.push_back(j);
+                   outputSum.push_back(i);
+                   outputSum.push_back(j);
                }
            }
        }
-        return output;
+        return outputSum;
     }
 };
