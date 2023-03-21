c094b40e6b3ddc51ea25ef701f849fba09673557
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-03-21 21:14:06 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (21.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int fibo_helper(int *ans,int n){
+    int fib_helper(int*ans,int n){
         if(n<=1){
             return n;
         }
         if(ans[n]!=-1){
             return ans[n];
         }
-        int a=fibo_helper(ans,n-1);
-        int b=fibo_helper(ans,n-2);
+        int a=fib_helper(ans,n-1);
+        int b=fib_helper(ans,n-2);
         ans[n]=a+b;
         return ans[n];
     }
     int fib(int n) {
-        int *ans=new int[n+1];
+        int* ans=new int[n+1];
         for(int i=0;i<=n;i++){
             ans[i]=-1;
         }
-        return fibo_helper(ans,n);
+        return fib_helper(ans,n);
     }
 };
