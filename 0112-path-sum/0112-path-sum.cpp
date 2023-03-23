56a9cf9b825742e0caff87a23df03ad2c616b7ee
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-03-23 10:19:24 +0530

    Time: 8 ms (86.32%), Space: 21.3 MB (76.65%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    bool hasPath(TreeNode* root,int targetSum,int curr){
+    bool hasPath(TreeNode* root,int targetSum){
         if(root==NULL){
             return false;
         }
-        curr+=root->val;
-        if((root->left==NULL && root->right==NULL) && targetSum==curr){
+        if((root->left==NULL && root->right==NULL) && targetSum-root->val==0){
             return true;
         }
         return 
-            (hasPath(root->left,targetSum,curr))||(hasPath(root->right,targetSum,curr));
+            (hasPath(root->left,targetSum-root->val))||(hasPath(root->right,targetSum-root->val));
       
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        int curr=0;
-        return hasPath(root,targetSum,curr);
+        return hasPath(root,targetSum);
     }
 };
