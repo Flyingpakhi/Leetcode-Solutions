7e8366b26282bae2cc33b1760294e2ca0073752e
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-03-27 09:45:55 +0530

    Time: 3 ms (72.03%), Space: 12.3 MB (17.58%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,42 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if(root==NULL){
-            return result;
-        }
-        queue<TreeNode*>q;
-        q.push(root);
-        bool LtoR=true;
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>ans(size);
-            for(int i=0;i<size;i++){
-                TreeNode* temp=q.front();
-                q.pop();
-                int index=LtoR?i:size-i-1;
-                ans[index]=temp->val;
-                if(temp->left){
-                    q.push(temp->left);
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                }
-                }
-            LtoR=!LtoR;
-            result.push_back(ans);
-        }
-        return result;
+        vector<vector<int>>result;
+    	if(root==NULL){
+    	    return result;
+    	}
+    	queue<TreeNode*>q;
+    	q.push(root);
+    	bool dir=true;
+    	while(!q.empty()){
+    	    int size=q.size();
+    	    vector<int>ans(size);
+    	    for(int i=0;i<size;i++){
+    	        TreeNode* frontNode=q.front();
+    	        q.pop();
+    	        int index=(dir)?i:size-i-1;
+    	        ans[index]=frontNode->val;
+    	        if(frontNode->left){
+    	            q.push(frontNode->left);
+    	        }
+    	        if(frontNode->right){
+    	            q.push(frontNode->right);
+    	        }
+    	    }
+    	    result.push_back(ans);
+    	    dir=!dir;
+    	}
+    	return result;
     }
 };
