eb383735b7d4092a668d6138e302c898e06a6102
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-04-01 10:25:28 +0530

    Time: 20 ms (53.38%), Space: 17.7 MB (50.50%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    // int neg=0,pos=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]>0){
-        //         pos++;
-        //     }
-        //     else if(nums[i]<0){
-        //         neg++;
-        //     }
-        // }
-        // return max(pos,neg);
     int maximumCount(vector<int>& nums) {
         int len=nums.size();
         //get the index of first pos number if 1 no then that index not just after 1 that index
