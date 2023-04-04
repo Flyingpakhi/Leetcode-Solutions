7bff15790ef53da6642fa43a3b05932d4db3357b
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-04-04 09:11:53 +0530

    Time: 477 ms (5.04%), Space: 72 MB (8.67%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void partition(map<char,bool>&seen,char a,string& st){
+        if(seen.find(a)!=seen.end()){
+            seen.clear();
+        }
+        else{
+            seen[a]=true;
+            st+=a;
+        }
+    }
+    int partitionString(string s) {
+        int size=s.length();
+        map<char,bool>seen;
+        string st="";
+        int ans=0;
+        for(int i=0;i<size;i++){
+            partition(seen,s[i],st);
+            if(seen.size()==0){
+                cout<<st<<endl;
+                ans++;
+                st="";
+                partition(seen,s[i],st);
+            }
+        }
+        if(seen.size()!=0){
+            ans++;
+        }
+        return ans;
+    }
+};
