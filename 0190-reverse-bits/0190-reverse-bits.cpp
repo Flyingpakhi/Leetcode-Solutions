93756bdcd878d1f9eea321c6fce9778f49e052a4
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-04-13 21:40:08 +0530

    Time: 6 ms (29.64%), Space: 6.1 MB (38.31%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+       for(int i=0;i<sizeof(int)*8;i++){
+           ans=ans<<1;
+           if(n&1==1){
+               ans=ans^1;
+           }
+            n=n>>1;
+       }
+        return ans;
+    }
+};
