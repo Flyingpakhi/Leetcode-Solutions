e9a4c4139cd3bcbe9a2579923191ebc42864c4a4
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-04-18 12:36:36 +0530

    Time: 4 ms (50.14%), Space: 11.5 MB (48.90%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -12,19 +12,15 @@ public:
         return num;
     }
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        int s1=val(event1[0]);
-        int e1=val(event1[1]);
-        int s2=val(event2[0]);
-        int e2=val(event2[1]);
-        if(s1>s2){
-            int temp=s1;
-            s1=s2;
-            s2=temp;
-            temp=e1;
-            e1=e2;
-            e2=temp;
+        int s2,e1;
+        if(val(event1[0])>val(event2[0])){
+            e1=val(event2[1]);
+            s2=val(event1[0]);
         }
-        cout<<s1<<" "<<e1<<" "<<s2<<" "<<e2<<endl;
+        else{
+            e1=val(event1[1]);
+            s2=val(event2[0]);
+        }
         if(e1>=s2){
             return true;
         }
