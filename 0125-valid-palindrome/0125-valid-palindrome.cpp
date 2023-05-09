7d3694b34405e7bf23aaa5e62305e1d543b7b6a7
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-09 09:01:02 +0530

    Time: 10 ms (23.44%), Space: 7.6 MB (52.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,49 +1,36 @@
 class Solution {
-    private:
-   //faltu character hata do..
-    bool validchar(char ch){
-        if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z')||(ch>='0' && ch<='9')){
-            return 1;
-        }
-        return 0;
-    }
-    
-int tolower_case(char ch){
-    if((ch>='a' && ch<='z')||(ch>='0'&& ch<='9')){
-        return ch;
-    }
-    else{
-        char tempu;
-        tempu=ch-'A'+'a';
-        return tempu;
-    }
-}
-bool check_pal(string name){
-        int check=1;
-        int k=name.length();
-for(int i=0;i<k/2;i++){
-    if(name[i]!=name[k-i-1]){
-        check=0;
-        break;
-    }
-}
-        return check;
-}
-    
-    
-
 public:
-    bool isPalindrome(string s) {
-        string temp="";
-        for(int i=0;i<s.length();i++){
-            if(validchar(s[i])){
-                temp.push_back(s[i]);
-            }
+char toLower(char ch){
+    char temp=ch-'A'+'a';
+    return temp;
+}
+bool pal(string &res){
+    int s=0;
+    int e=res.size()-1;
+    while(s<=e){
+        if(res[s]!=res[e]){
+            return false;
         }
-        for(int i=0;i<temp.length();i++){
-            temp[i]=tolower_case(temp[i]);
-        }
-            
-         return check_pal(temp);   
+        s++;
+        e--;
     }
+    return true;
+}
+bool isPalindrome(string s) {
+    string res="";
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='a'&&s[i]<='z'){
+            res+=s[i];
+        }
+        else if(s[i]>='A'&&s[i]<='Z'){
+            char temp=toLower(s[i]);
+            res+=temp;
+        }
+        else if(s[i]>='0'&&s[i]<='9'){
+            res+=s[i];
+        }
+    }
+    bool ans=pal(res);
+    return ans;
+}
 };
