1bb62e6ac71141ebf6666b2efacc81ee4837611d
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-10 09:06:00 +0530

    Time: 4 ms (33.05%), Space: 6.5 MB (70.13%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-      vector<vector<int>>matrix1;
-        vector<int> output;
-        int matrix[100][100];
+      vector<vector<int>>matrix(n,vector<int>(n,0));
+        // vector<int> output;
+        // int matrix[100][100];
         int k=0;
         int row_start=0,row_end=n-1,col_start=0,col_end=n-1;
     while(row_start<=row_end && col_start<=col_end){
@@ -40,13 +40,13 @@ public:
             break;
         }
     }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                output.push_back(matrix[i][j]);
-            }
-            matrix1.push_back(output);
-            output.clear();
-        }
-        return matrix1;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         output.push_back(matrix[i][j]);
+        //     }
+        //     matrix1.push_back(output);
+        //     output.clear();
+        // }
+        return matrix;
     }
 };
