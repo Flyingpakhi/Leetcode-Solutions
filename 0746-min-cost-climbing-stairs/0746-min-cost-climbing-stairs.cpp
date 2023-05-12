49b9b1b8557f03ced1b837554e1b507a3533103e
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-12 10:51:09 +0530

    Time: 10 ms (31.91%), Space: 13.8 MB (53.56%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    // int solve(vector<int>&cost,vector<int>&dp,int n){
-    //     if(n==0){
-    //         return cost[0];
-    //     }
-    //     if(n==1){
-    //         return cost[1];
-    //     }
-    //     //if already value is stored then just return
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-    //     //store the value in dp array for further use
-    //     //when we are in kth stair then to further go we have to pay the cost of kth stair..so to reach kth stair min cost will be min of k-1 and k-2 cost with addition of kth cost
-    //     dp[n]=min(solve(cost,dp,n-1),solve(cost,dp,n-2))+cost[n];
-    //     return dp[n];
-    // }
-    // int minCostClimbingStairs(vector<int>& cost) {
-    //     int n=cost.size();
-    //     //make an array of n+1 and initialize with 1
-    //     vector<int>dp(n+1,-1);
-    //     //in the top floor we do not go further so need not to pay the cost
-    //     int ans=min(solve(cost,dp,n-1),solve(cost,dp,n-2));
-    //     return ans;
-    // }
-    int minCostClimbingStairs(vector<int>& cost){
+    int solve(vector<int>&cost,vector<int>&dp,int n){
+        if(n==0){
+            return cost[0];
+        }
+        if(n==1){
+            return cost[1];
+        }
+        //if already value is stored then just return
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        //store the value in dp array for further use
+        //when we are in kth stair then to further go we have to pay the cost of kth stair..so to reach kth stair min cost will be min of k-1 and k-2 cost with addition of kth cost
+        dp[n]=min(solve(cost,dp,n-1),solve(cost,dp,n-2))+cost[n];
+        return dp[n];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        // int n=cost.size();
+        // //make an array of n+1 and initialize with 1
+        // vector<int>dp(n+1,-1);
+        // //in the top floor we do not go further so need not to pay the cost
+        // int ans=min(solve(cost,dp,n-1),solve(cost,dp,n-2));
+        // return ans;
         int n=cost.size();
         vector<int>dp(n+1);
         dp[0]=cost[0];
