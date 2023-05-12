664661862d4c7fb8fc1076cb87aa0fcff9a89e98
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-12 10:43:33 +0530

    Time: 11 ms (26.41%), Space: 13.9 MB (30.52%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,15 +7,20 @@ public:
         if(n==1){
             return cost[1];
         }
+        //if already value is stored then just return
         if(dp[n]!=-1){
             return dp[n];
         }
+        //store the value in dp array for further use
+        //when we are in kth stair then to further go we have to pay the cost of kth stair..so to reach kth stair min cost will be min of k-1 and k-2 cost with addition of kth cost
         dp[n]=min(solve(cost,dp,n-1),solve(cost,dp,n-2))+cost[n];
         return dp[n];
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
+        //make an array of n+1 and initialize with 1
         vector<int>dp(n+1,-1);
+        //in the top floor we do not go further so need not to pay the cost
         int ans=min(solve(cost,dp,n-1),solve(cost,dp,n-2));
         return ans;
     }
