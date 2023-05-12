4746881e69b1c160023ba3f1f5ae4e9ea94edc2c
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-12 10:58:43 +0530

    Time: 10 ms (31.91%), Space: 13.5 MB (96.55%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int solve(vector<int>&cost,vector<int>&dp,int n){
+    //using recursion + memoization
+    /*int solve(vector<int>&cost,vector<int>&dp,int n){
         if(n==0){
             return cost[0];
         }
@@ -17,19 +18,43 @@ public:
         return dp[n];
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        // int n=cost.size();
-        // //make an array of n+1 and initialize with 1
-        // vector<int>dp(n+1,-1);
-        // //in the top floor we do not go further so need not to pay the cost
-        // int ans=min(solve(cost,dp,n-1),solve(cost,dp,n-2));
-        // return ans;
+        int n=cost.size();
+        //make an array of n+1 and initialize with 1
+        vector<int>dp(n+1,-1);
+        //in the top floor we do not go further so need not to pay the cost
+        int ans=min(solve(cost,dp,n-1),solve(cost,dp,n-2));
+        return ans;
+    }*/
+    //using tabulation method bottom up approach\
+    
+    /*int minCostClimbingStairs(vector<int>& cost){
         int n=cost.size();
         vector<int>dp(n+1);
+        //base case analyse
         dp[0]=cost[0];
         dp[1]=cost[1];
+        //handle the case from 2 to n-1
         for(int i=2;i<n;i++){
             dp[i]=min(dp[i-1],dp[i-2])+cost[i];
         }
+        //nth case
         return min(dp[n-1],dp[n-2]);
+    }*/
+    //using space optimization
+    
+    int minCostClimbingStairs(vector<int>& cost){
+        int n=cost.size();
+        //base case analyse
+        int prev1=cost[0];
+        int prev2=cost[1];
+        int curr;
+        //handle the case from 2 to n-1
+        for(int i=2;i<n;i++){
+            curr=min(prev1,prev2)+cost[i];
+            prev1=prev2;
+            prev2=curr;
+        }
+        //nth case
+        return min(prev2,prev1);
     }
 };
