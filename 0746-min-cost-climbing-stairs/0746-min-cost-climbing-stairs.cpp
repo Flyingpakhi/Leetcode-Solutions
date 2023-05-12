b626f4e0245e384a7dded1495e2b75fc7f521a60
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-12 10:51:31 +0530

    Time: 7 ms (65.68%), Space: 13.8 MB (30.52%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -30,7 +30,6 @@ public:
         for(int i=2;i<n;i++){
             dp[i]=min(dp[i-1],dp[i-2])+cost[i];
         }
-        dp[n]=min(dp[n-1],dp[n-2]);
-        return dp[n];
+        return min(dp[n-1],dp[n-2]);
     }
 };
