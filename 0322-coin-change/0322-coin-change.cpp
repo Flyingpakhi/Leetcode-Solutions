7740bc1d04a50b3322990d66633706a80573a51c
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-13 12:35:19 +0530

    Time: 205 ms (10.25%), Space: 14.4 MB (82.10%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
+                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        if(dp[amount]==INT_MAX){
+            return -1;
+        }
+        return dp[amount];
+    }
+};
