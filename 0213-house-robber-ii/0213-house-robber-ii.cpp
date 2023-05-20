6c777fae7468073442b3f30a994e277918722d55
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-20 19:02:53 +0530

    Time: 6 ms (16.40%), Space: 8.1 MB (18.98%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    int robbing(vector<int>&nums,int k,int len){
-        int rob1=0,rob2=0;
-        for(int i=k;i<len;i++){
-            int temp=max(nums[i]+rob1,rob2);
-            rob1=rob2;
-            rob2=temp;
+    int robb(vector<int> nums) {
+        int n=nums.size();
+        int rob1=nums[0];
+        int rob2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1){
+                take+=rob2;
+            }
+            int nt=rob1+0;
+            int curr=max(take,nt);
+            rob2=rob1;
+            rob1=curr;
         }
-        return rob2;
+        return rob1;
     }
     int rob(vector<int>& nums) {
-        int len=nums.size();
-        if(len==1){
+        int n=nums.size();
+        if(n==1){
             return nums[0];
         }
-        int count1=robbing(nums,0,len-1);
-        int count2=robbing(nums,1,len);
-        if(count1>count2){
-            return count1;
+        vector<int>temp,temp1;
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                temp.push_back(nums[i]);
+            }
+            if(i!=n-1){
+                temp1.push_back(nums[i]);
+            }
         }
-        else{
-            return count2;
-        }
+        return max(robb(temp),robb(temp1));
     }
 };
