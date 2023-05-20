6aaf8cbf1ee8b5e295ffc3e098a5394bb025cd94
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-05-20 18:25:52 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (82.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int rob1=0,rob2=0;
-        for(int i=0;i<nums.size();i++){
-            int temp=max(nums[i]+rob1,rob2);
-            rob1=rob2;
-            rob2=temp;
+        int n=nums.size();
+        int rob1=nums[0];
+        int rob2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1){
+                take+=rob2;
+            }
+            int nt=rob1+0;
+            int curr=max(take,nt);
+            rob2=rob1;
+            rob1=curr;
         }
-        return rob2;
+        return rob1;
     }
 };
