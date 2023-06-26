d05cd8a3735bccc46655152a90bcd1ad728a9b22
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-06-26 10:19:27 +0530

    Time: 29 ms (44.34%), Space: 19.1 MB (98.40%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //implies the position of nonZero element
-        int nonZero=0;
-        for(int j=0;j<nums.size();j++){
-            //find nonzero element and swap it with the value in its correct position
-            if(nums[j]!=0){
-                swap(nums[j],nums[nonZero]);
-                nonZero++;
+        int index=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
             }
+        }
+        for(int i=index;i<nums.size();i++){
+            nums[i]=0;
         }
     }
 };
