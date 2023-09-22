2ff4956a4ef486d4ec780ac3b26f463e899bca8e
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2023-09-23 00:11:54 +0530

    Time: 2 ms (55.77%), Space: 6.2 MB (8.21%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return (a|b)+(a&b);
+        while(b!=0){
+            int temp=(a&b)<<1;
+            a=a^b;
+            b=temp;
+        }
+        return a;
     }
 };
