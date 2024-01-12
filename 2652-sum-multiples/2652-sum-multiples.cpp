09aa4104dfaad77f187d37820cad94a22477d99d
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-01-12 15:04:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (98.61%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        long long sum=0;
+        for(int i=3;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0){
+                sum+=i;
+            }
+        }
+        return (int)sum;
+        
+    }
+};
