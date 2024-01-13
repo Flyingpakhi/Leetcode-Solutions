e505aba6470e69a3712ec6d7c16529324c900da1
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-01-13 14:27:47 +0530

    Time: 5 ms (35.96%), Space: 9 MB (22.42%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int cit(vector<int>&v,int h){
+        int count=0;
+        for(int i=0;i<v.size();i++){
+            if(v[i]>=h){
+                count++;
+            }
+        }
+        return count;
+    }
+    int hIndex(vector<int>& citations) {
+        int low=0;
+        int high=citations.size();
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int count=cit(citations,mid);
+            if(count>=mid){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
