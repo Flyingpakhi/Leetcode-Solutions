292929912a799fc0f7b272f7cdf9f44116f1cbb2
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-01-13 15:51:27 +0530

    Time: 15 ms (54.05%), Space: 18.9 MB (87.60%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lower(vector<int>&c,int val){
+        int low=0,high=c.size()-1;
+        int ans=c.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(c[mid]>=val){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return c.size()-ans;
+        
+    }
+    int hIndex(vector<int>& citations) {
+        
+        int low=0;
+        int high=citations.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            int count=lower(citations,mid);
+            if(count>=mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
