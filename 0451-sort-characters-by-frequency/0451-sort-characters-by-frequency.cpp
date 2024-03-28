06e09c844ef0ee7d742022d2b16683c045ffbb68
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-03-28 12:42:39 +0530

    Time: 11 ms (53.26%), Space: 10.8 MB (69.62%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -9,15 +9,20 @@ public:
 class Solution {
 public:
     string frequencySort(string s) {
+//         declare an empty string
         string st="";
+//         declare an u_map seen to store the frequency of each char of the string
         unordered_map<char,int>seen;
         for(int i=0;i<s.size();i++){
             seen[s[i]]++;
         }
+// declare a priority queue->sorting done on the basis of second elements of pq
+        //max heap->top element->max value from second elements of pq
         priority_queue<pair<char,int>,vector<pair<char,int>>,Compare>pq;
         for(auto it:seen){
             pq.push({it.first,it.second});
         }
+//         pop out the highest frequency element and store it in string
         while(!pq.empty()){
             for(int i=0;i<pq.top().second;i++){
                 st+=pq.top().first;
