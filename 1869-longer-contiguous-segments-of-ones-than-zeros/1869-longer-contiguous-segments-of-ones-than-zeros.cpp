4a657b632c03bf3b0807629b8437f614de77df62
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-04-19 12:14:30 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (88.01%) - LeetHub

---
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int count=0;
+        int count1=0;
+        int maxi=-1;
+        int maxi1=-1;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+                if(count>maxi){
+                    maxi=count;
+                }
+                count=0;
+                count1++;
+            }
+            else{
+                if(count1>maxi1){
+                    maxi1=count1;
+                }
+                count1=0;
+                count++;
+            }
+        }
+        if(count>maxi){
+                    maxi=count;
+        }
+        if(count1>maxi1){
+                    maxi1=count1;
+        }
+        
+//         maxi->max cons 1, maxi1->max cons 0
+        cout<<maxi<<" "<<maxi1<<endl;
+        if(maxi>maxi1){
+            return true;
+        }
+        return false;
+    }
+};
