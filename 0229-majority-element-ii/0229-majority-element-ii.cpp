cc20f12ade7603f9d3a9716add8e38051f18156e
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-04-22 12:29:11 +0530

    Time: 16 ms (7.94%), Space: 18.7 MB (32.75%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>res;
-        unordered_map<int,int>ans;
+        map<int,int>ans;
         for(int i=0;i<nums.size();i++){
             ans[nums[i]]++;
+            if(ans.size()<=2){
+                continue;
+            }
+            map<int,int>count;
+            for(auto i:ans){
+                if(i.second>1){
+                    count[i.first]=i.second-1;
+                }
+            }
+            ans=count;
         }
+        // for(auto i:ans){
+        //     cout<<i.first<<" ";
+        // }
+        vector<int>res;
+        int c;
         for(auto i:ans){
-            if(i.second>nums.size()/3.0){
+            c=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]==i.first){
+                    c++;
+                }
+            }
+            if(c>nums.size()/3.0){
                 res.push_back(i.first);
             }
         }
