8e89341c93778678702e4cbd21ba7f55b75ab7bc
flying pakhi <112820414+Flyingpakhi@users.noreply.github.com>
2024-05-03 00:32:27 +0530

    Time: 17 ms (79.03%), Space: 27.3 MB (67.43%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int>arr;
-        arr.push_back(first);
-        for(int i=0;i<encoded.size();i++){
-            int ans=arr[i]^encoded[i];
-            arr.push_back(ans);
+        encoded.insert(encoded.begin(),first);
+        for(int i=1;i<encoded.size();i++){
+            encoded[i]=encoded[i-1]^encoded[i];
         }
-        return arr;
+        return encoded;
     }
 };
